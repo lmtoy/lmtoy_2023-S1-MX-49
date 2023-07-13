6f29e89cb59d29d051b75b812d1ac92e084e84e8
Peter Teuben <teuben@gmail.com>
2023-07-12 21:27:15 -0400

    alternative method

--- mk_runs.py
+++ mk_runs.py
@@ -17,13 +17,19 @@ on["L1157-B1"] =  [ 109989, 109990,]                                        # ma
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
-pars1["L1157-B1"] = ""
+pars1["L1157-B1"] = "extent=200 dv=20 dw=20"
 
 #        common parameters per source on subsequent runs (run1b, run2b)
 pars2 = {}
-pars2["L1157-B1"] = "pix_list=-5,12,13"
-# bank0: -5,12,13
-# bank1: -0,2,3,4,8,9,14,15#
+pars2["L1157-B1"] = "oid=0 bank=0 pix_list=-5,12,13"
+
+#        common parameters per source on subsequent runs (run1c, run2c)
+pars3 = {}
+pars3["L1157-B1"] = "oid=1 bank=1 pix_list=-0,2,3,4,8,9,14,15"
+
+#        or this
+pars2_test = ["oid=0 bank=0 pix_list=-5,12,13",
+              "oid=1 bank=1 pix_list=-0,2,3,4,8,9,14,15"]
 
 if __name__ == '__main__':    
-    runs.mk_runs(project, on, pars1, pars2)
+    runs.mk_runs(project, on, pars1, pars2, sys.argv)
